8872aa33713a57c9c239aaab11807d1ebdf830d3
Andras Zolyomi <andras.zolyomi@gmail.com>
2022-10-09 12:44:35 -0600

    Change solution text on Counter.jsx

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -24,7 +24,7 @@ const Counter = () => {
     <div className="counter-outer-wrapper">
         <div className="counter-wrapper">
             <h1>Counter</h1>
-            <p>The <span className="bold">Counter</span> component is broken. Fix it to complete the lab.</p>
+            <p>This <span className="bold">Counter</span> component is correct.</p>
             <div className="counter-row">
                 <CounterButton increment={-1} updateCount={updateCount} />
                 <div className="counter-value">{count}</div>
